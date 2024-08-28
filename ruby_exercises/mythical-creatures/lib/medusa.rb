777ba5f257ec89806dbd280b2b7ad249298b4e69
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-28 17:50:51 -0400

    Creates and initializes the medusa.rb file

---
+++ ruby_exercises/mythical-creatures/lib/medusa.rb
@@ -0,0 +1,26 @@
+class Medusa
+
+    def initialize(name)
+        @name = name
+        @statues = []
+    end
+
+    def name
+        @name
+    end
+
+    def statues
+        @statues
+    end
+end
+
+class Person
+
+    def initialize(person)
+        @nperson = person
+    end
+
+    def person
+        @person
+    end
+end
