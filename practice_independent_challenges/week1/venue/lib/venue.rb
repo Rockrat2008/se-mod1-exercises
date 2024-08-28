6adb450b3d9b4943532d61ebed765cce255336e5
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-28 15:22:07 -0400

    Adds the functions for the spec file tests

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,27 @@
 class Venue
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def name
+        @name
+    end
+
+    def capacity
+        @capacity
+    end
+
+    def patrons
+        @patrons
+    end
+
+    def add_patron(patron_name)
+        @patrons << patron_name
+    end
+
+    def yell_at_patrons
+        @patrons.map { |patron| patron.upcase}
+    end
 end
