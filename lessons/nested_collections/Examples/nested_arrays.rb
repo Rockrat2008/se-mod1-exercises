1888273d487525bf64911fa8a839b554b237f85a
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-29 12:49:37 -0400

    completed lessons

--- lessons/nested_collections/Examples/nested_arrays.rb
+++ lessons/nested_collections/Examples/nested_arrays.rb
@@ -1,7 +1,13 @@
 numbers = [[1, 2, 3], [4, 5, 6]]
 
-# 1. what is numbers.count
-# 2. what is numbers.first.count
+# 1. what is numbers.count (2)
+puts numbers.count
+# 2. what is numbers.first.count (3)
+puts numbers.first.count
 # 3. how can I access the element 5
+puts numbers.last[1]
 # 4. how can I add [7,8,9] to the numbers array
-# 5. What element does numbers[0] return?
+numbers,push[[7, 8, 9])
+puts numbers
+# 5. What element does numbers[0] return? [1, 2, 3]
+puts numbers[0]
