78fd5d0532978c5c7acaf9f394a05dd9d3b09c45
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-29 12:51:10 -0400

    Completed lessons

--- lessons/nested_collections/Examples/hashes_within_arrays.rb
+++ lessons/nested_collections/Examples/hashes_within_arrays.rb
@@ -1,6 +1,11 @@
 food_feelings = [{:pizza => "tasty"}, {:calzone => "also tasty"}]
 
-# 1. what is food_feelings.count
-# 2. what is food_feelings.first.count
+# 1. what is food_feelings.count (2)
+puts food_feelings.count
+# 2. what is food_feelings.first.count (1)
+put food_feelings.first.count
 # 3. how can I access the value "also tasty"
+puts food_feelings.last[:calzone]
 # 4. how can I change the value “also tasty” to “super delicious”
+food_feelings.last[:calzone] = "super delicious"
+puts food_feelings
