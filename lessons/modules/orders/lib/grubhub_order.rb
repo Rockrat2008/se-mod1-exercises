21229571f69cb206b473c5a8abb01c6ad621b2fc
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-10 11:48:50 -0400

    Updates files to use a method

--- lessons/modules/orders/lib/grubhub_order.rb
+++ lessons/modules/orders/lib/grubhub_order.rb
@@ -1,14 +1,11 @@
 # ./grubhub_order.rb
+require './lib/online_order'
+
 class GrubhubOrder
-  def confirmation(thing)
-    "You got #{thing}."
-  end
+  include OnlineOrder
 
   def delivery
     "Your food will arrive in 45-60 minutes."
   end
 
-  def review
-    "Please rate your order within 30 days."
-  end
 end
